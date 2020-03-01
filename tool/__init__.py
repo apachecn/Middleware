f0bb8469ce9a6ed4038458dc078a95e1a0a1a288
jiangzhonglian <jiang-s@163.com>
2020-03-01 17:51:37 +0800

    修改目录名称

---
+++ tool/__init__.py
@@ -0,0 +1,4 @@
+# coding: utf8
+from tool.u_file import *
+from tool.u_text import *
+from tool.u_mail import *
--- u_tool/__init__.py
+++
@@ -1,4 +0,0 @@
-# coding: utf8
-from u_tool.u_file import *
-from u_tool.u_text import *
-from u_tool.u_mail import *
